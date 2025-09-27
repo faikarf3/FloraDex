69713a194014fc9333aa42ed442c0e8c2b768361
Makiya <107805761+makiya32@users.noreply.github.com>
2025-09-26 20:19:05 -0400

    tab layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,15 @@
-import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        {/* Tabs group (main dashboard + other tabs) */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-        {/* Modal screen */}
-        <Stack.Screen
-          name="add-plant-modal"
-          options={{
-            presentation: 'modal', // 👈 makes it slide up like Twitter compose
-            headerShown: false,
-          }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="modal"
+        options={{
+          presentation: 'modal',
+        }}
+      />
+    </Stack>
   );
 }
